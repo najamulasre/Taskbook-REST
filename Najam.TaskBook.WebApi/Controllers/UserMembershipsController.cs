373cfb52d7fc2fc15e5b92f9b00436c595b6d6fe
Khan Najam ul Asre <21303779@student.uwl.ac.uk>
2018-02-10 22:35:02 +0000

    code cleanup

--- Najam.TaskBook.WebApi/Controllers/UserMembershipsController.cs
+++ Najam.TaskBook.WebApi/Controllers/UserMembershipsController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
